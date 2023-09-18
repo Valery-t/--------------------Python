c4e871021d93fc9278d4122a4b5d34d6ed2b994f
Valery <tereshonok.v@gmail.com>
2023-09-18 23:13:39 +0300

    функции завершены

--- Семинар 8  Работа с файлами/Семинар 8 Работа с файлами.py
+++ Семинар 8  Работа с файлами/Семинар 8 Работа с файлами.py
@@ -24,29 +24,30 @@ def show_menu():
 def work_with_phonebook():
     choice = show_menu()
     phone_book = read_txt("phone.txt")
-    write_txt("phone.txt",phone_book)
     while choice != 7:
         if choice == 1:
-            print_result(phone_book)
+            print([print_result(phone_book)])
         elif choice == 2:
-            last_name = input("Фамилия ")
+            last_name = input("Фамилия: ")
             print(find_by_lastname(phone_book, last_name))
         elif choice == 3:
-            last_name = input("Фамилия ")
-            new_number = input("new number ")
-            print(change_number(phone_book, last_name, new_number))
+            last_name = input("Фамилия: ")
+            new_number = input("Новый номер: ")
+            phone_book = change_number(phone_book, last_name, new_number)
+            print(f'Номер телефона изменен {write_txt("phone.txt",phone_book)}')
         elif choice == 4:
-            lastname = input("Фамилия ")
-            print(delete_by_lastname(phone_book, lastname))
+            lastname = input("Фамилия: ")
+            phone_book = delete_by_lastname(phone_book, lastname)
+            print(f'Абонент удален {write_txt("phone.txt",phone_book)}')
         elif choice == 5:
-            number = input("number ")
+            number = input("Номер: ")
             print(find_by_number(phone_book, number))
         elif choice == 6:
-            #user_data = input("new data ")
-            # print(add_user(phone_book,user_data()))
-            write_user("phone.txt")
+            data = add_data()
+            print(f'Данные внесены {write_user("phone.txt",data)}')
+            phone_book = read_txt("phone.txt")
+
         choice = show_menu()
-        
 
 
 def read_txt(filename):
@@ -54,12 +55,14 @@ def read_txt(filename):
     fields = ["Фамилия", "Имя", "Телефон", "Описание"]
     with open(filename, "r", encoding="utf-8") as phb:
         for line in phb:
-            record = dict(zip(fields, line.strip().split(",")))  # dict(((фамилия, Иванов), (имя, Точка), (номер, 8928)))
+            record = dict(
+                zip(fields, line.strip().split(","))
+            )  # dict(((фамилия, Иванов), (имя, Точка), (номер, 8928)))
             phone_book.append(record)
     return phone_book
 
 
-def write_txt(filename,phone_book):
+def write_txt(filename, phone_book):
     with open(filename, "w", encoding="utf-8") as phout:
         for i in range(len(phone_book)):
             s = ""
@@ -76,44 +79,40 @@ def print_result(phone_book):  # 1. Распечатать справочник
 def find_by_lastname(phone_book, last_name):  # 2. Найти телефон по фамилии
     for dict in phone_book:
         if dict["Фамилия"] == last_name:
-            print(dict.get("Телефон"))
-            
+            print(f'Номер телефона: {dict.get("Телефон")}')
 
 
 def change_number(phone_book, last_name, new_number):  # 3. Изменить номер телефона
     for dict in phone_book:
-        if dict["Фамилия"] == str(last_name):
+        if dict["Фамилия"] == last_name:
             dict["Телефон"] = new_number
-    print(f'{write_txt("phone.txt",phone_book)} телефон изменен')
-            
-def delete_by_lastname(phone_book, lastname):   #4. Удалить запись
+    return phone_book
+
+
+def delete_by_lastname(phone_book, lastname):  # 4. Удалить запись
     for dict in phone_book:
         if dict["Фамилия"] == lastname:
-            del dict
-    print(f'{write_txt("phone.txt",phone_book)} запись удалена')
+            del dict["Фамилия"], dict["Имя"], dict["Телефон"], dict["Описание"]
+    return phone_book
+
 
 def find_by_number(phone_book, number):  # 5. Найти абонента по номеру телефона
     for dict in phone_book:
         if dict["Телефон"] == number:
-            print(dict.get("Фамилия"))
+            print(f'Фамилия: {dict.get("Фамилия")}')
 
 
-def write_user(filename):                    # 6. Добавить абонента в справочник
+def add_data():
     surname = str(input("Введите фамилию: "))
     name = str(input("Введите имя: "))
     phone = int(input("Введите телефон: "))
     describe = str(input("Введите описание: "))
-    with open(filename, 'a', encoding='utf-8') as file:
-        file.write(f"{surname},{name},{phone},{describe}\n")
+    return surname, name, phone, describe
 
-def delete_by_lastname(phone_book, lastname):
-    for dict in phone_book:
-        if lastname in dict:
-            del dict["lastname"]
-            print(f"Элемент с ключом {lastname} удален")
-        else:
-            print("Элемент не найден")
 
+def write_user(filename, data):  # 6. Добавить абонента в справочник
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(f"{data[0]},{data[1]},{data[2]},{data[3]}\n")
 
 
 work_with_phonebook()
