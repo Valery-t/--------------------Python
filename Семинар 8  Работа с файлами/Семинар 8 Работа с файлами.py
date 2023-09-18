9ff67ebefec9807d1bc8325ea54c5cddc35f711b
Valery <tereshonok.v@gmail.com>
2023-09-18 16:36:42 +0300

    обновил п1.2.3.6

--- Семинар 8  Работа с файлами/Семинар 8 Работа с файлами.py
+++ Семинар 8  Работа с файлами/Семинар 8 Работа с файлами.py
@@ -24,28 +24,29 @@ def show_menu():
 def work_with_phonebook():
     choice = show_menu()
     phone_book = read_txt("phone.txt")
-    phone_book_w = write_txt("phone.txt", phone_book)
+    write_txt("phone.txt",phone_book)
     while choice != 7:
         if choice == 1:
             print_result(phone_book)
         elif choice == 2:
-            last_name = input("lastname ")
+            last_name = input("Фамилия ")
             print(find_by_lastname(phone_book, last_name))
         elif choice == 3:
-            last_name = input("lastname ")
+            last_name = input("Фамилия ")
             new_number = input("new number ")
-            print(change_number(phone_book_w, last_name, new_number))
+            print(change_number(phone_book, last_name, new_number))
         elif choice == 4:
-            lastname = input("lastname ")
+            lastname = input("Фамилия ")
             print(delete_by_lastname(phone_book, lastname))
         elif choice == 5:
             number = input("number ")
             print(find_by_number(phone_book, number))
         elif choice == 6:
-            user_data = input("new data ")
-            add_user(phone_book, user_data)
-            write_txt("phonebook.txt", phone_book)
+            #user_data = input("new data ")
+            # print(add_user(phone_book,user_data()))
+            write_user("phone.txt")
         choice = show_menu()
+        
 
 
 def read_txt(filename):
@@ -53,14 +54,12 @@ def read_txt(filename):
     fields = ["Фамилия", "Имя", "Телефон", "Описание"]
     with open(filename, "r", encoding="utf-8") as phb:
         for line in phb:
-            record = dict(
-                zip(fields, line.split(","))
-            )  # dict(((фамилия, Иванов), (имя, Точка), (номер, 8928)))
+            record = dict(zip(fields, line.strip().split(",")))  # dict(((фамилия, Иванов), (имя, Точка), (номер, 8928)))
             phone_book.append(record)
     return phone_book
 
 
-def write_txt(filename, phone_book):
+def write_txt(filename,phone_book):
     with open(filename, "w", encoding="utf-8") as phout:
         for i in range(len(phone_book)):
             s = ""
@@ -71,30 +70,41 @@ def write_txt(filename, phone_book):
 
 def print_result(phone_book):  # 1. Распечатать справочник
     for dict in phone_book:
-        x = [*dict.values()]
-        print(x)
+        print(dict)
 
 
 def find_by_lastname(phone_book, last_name):  # 2. Найти телефон по фамилии
     for dict in phone_book:
-        if dict["Фамилия"] == str(last_name):
+        if dict["Фамилия"] == last_name:
             print(dict.get("Телефон"))
-            break
+            
 
 
 def change_number(phone_book, last_name, new_number):  # 3. Изменить номер телефона
     for dict in phone_book:
         if dict["Фамилия"] == str(last_name):
-            dict["Фамилия"] = int(new_number)
-            print(dict["Фамилия"])
-            break
-
+            dict["Телефон"] = new_number
+    print(f'{write_txt("phone.txt",phone_book)} телефон изменен')
+            
+def delete_by_lastname(phone_book, lastname):   #4. Удалить запись
+    for dict in phone_book:
+        if dict["Фамилия"] == lastname:
+            del dict
+    print(f'{write_txt("phone.txt",phone_book)} запись удалена')
 
 def find_by_number(phone_book, number):  # 5. Найти абонента по номеру телефона
     for dict in phone_book:
-        if dict["Телефон"] == int(number):
+        if dict["Телефон"] == number:
             print(dict.get("Фамилия"))
 
+
+def write_user(filename):                    # 6. Добавить абонента в справочник
+    surname = str(input("Введите фамилию: "))
+    name = str(input("Введите имя: "))
+    phone = int(input("Введите телефон: "))
+    describe = str(input("Введите описание: "))
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.write(f"{surname},{name},{phone},{describe}\n")
 
 def delete_by_lastname(phone_book, lastname):
     for dict in phone_book:
@@ -104,9 +114,6 @@ def delete_by_lastname(phone_book, lastname):
         else:
             print("Элемент не найден")
 
-
-def add_user(phone_book, user_data):
-    re
 
 
 work_with_phonebook()
